054283032f178e24de053e5fa857fbae7a23c6c4
s-takuya <your.takuyashiratake0622@icloud.com>
2014-12-09 16:49:05 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 	before_action :signed_in_user,only: [:index,:edit, :update]
 	before_action :correct_user,  only: [:edit, :update]
 	before_action :admin_user,   only: :destroy
+  def index
+    @users = User.all
+  end
   def show
   	@user = User.find(params[:id])
   	@microposts = @user.microposts.paginate(page: params[:page])
@@ -32,6 +35,26 @@ class UsersController < ApplicationController
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
+   def edit
+    @user = User.find(params[:id])
+  
+  end 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render'edit'
+    end
+  end
+  def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User destroyed."
+  redirect_to users_url
+end
+
+
 
 
 
@@ -41,18 +64,11 @@ class UsersController < ApplicationController
   def user_params
   	params.require(:user).permit(:name,:email,:age,:occupation,:password,:password_confirmation)
   	end
-  def edit
-  
-  end	
-  def update
-  	@user = User.find(params[:id])
-  	if @user.update_attributes(user_params)
-  		flash[:success] = "Profile updated"
-  		redirect_to @user
-  	else
-  		render'edit'
-  	end
-  end
+  def signed_in_user
+  redirect_to signin_url, notice:"Please sign in." unless signed_in? 
+  end 
+
+
   
   def correct_user
   	@user = User.find(params[:id])
@@ -61,7 +77,7 @@ class UsersController < ApplicationController
   def index 
   	@users = User.paginate(page: params[:page])
   end
-end
+
 def destroy
 	User.find(params[:id]).destroy
 	flash[:success] = "User destroyed."
@@ -69,6 +85,7 @@ def destroy
 end
 def admin_user
 	redirect_to(root_path)unless current_user.admin?
+end
 end
 
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do 
 	factory :user do
 		sequence(:name) { |n| "Person#{n}" }
-		age " "
-		occupation " "
+		
 		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
