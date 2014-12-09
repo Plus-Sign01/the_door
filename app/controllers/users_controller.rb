8ff3dc01813275173b68a67d3991f8c48812cb1d
s-takuya <your.takuyashiratake0622@icloud.com>
2014-12-09 13:40:02 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		sign_in@user
-  		flash[:success] = "Welcome to the The Door"
+  		flash[:success] = "Welcome to The Door"
   		redirect_to @user
   	else
   		render'new'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,8 @@
 	module UsersHelper
 	def gravatar_for(user,options = { size: 50 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravater_url = "https://secure.gravatar.com/avatar/#{gravater_id}"
+		size = options[:size]
+		gravater_url = "https://secure.gravatar.com/avatar/#{gravater_id}?s=#{size}"
 		image_tag(gravatar_url,alt: user.name, class: "gravater")
 	end
 end
