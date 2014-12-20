47e975d89b8adc04848735e29ada5d148927ae48
s-takuya <your.takuyashiratake0622@icloud.com>
2014-12-20 21:12:23 +0900

    changed model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,16 @@ class SessionsController < ApplicationController
 	def new
 	end
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user
-	else
-		flash.now[:error] = 'Invalid email/password combination'
-		render 'new'
+		user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+		session[:user_id] = user.id
+		redirect_to root_path, notice: 'ログインしました'
 	end
-end
 
-		
 	
 	def destroy
-		sign_out
-		redirect_to root_url
+		reset_session
+		redirect_to root_path, notice: 'ログアウトしました'
+	
 	end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,21 +9,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/render_demo'
   get 'pages/redirect_demo'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
 
-  resources :users do
-    member do
-      get :following,:followers
-    end
+
   end
-
-  resources :sessions,only: [:new,:create,:destroy]
-  resources :microposts,only: [:create, :destroy]
-  resources :relationships, only:[:create,:destroy]
-  root  'static_pages#home'
-  match '/signup', to: 'users#new',  via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signout',to:'sessions#destroy',    via: 'delete'
-  match '/signin', to:'sessions#new',   via: 'get'
-end
