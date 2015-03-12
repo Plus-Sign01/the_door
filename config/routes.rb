00d7c9ad74cb3e5f6012026a5b5a9b097ba3b1f9
s-takuya <takuyashiratake0622@icloud.com>
2015-03-12 10:10:51 +0900

    jfjf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TheDoor::Application.routes.draw do
 
+  devise_for :users
   root to: 'welcome#index'
   get '/login' => 'sessions#create', as: :login, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
